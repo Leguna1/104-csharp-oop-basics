f3873c6d1a11359b9acdfc7c8e74c5ed849bf6a1
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-10-10 11:32:42 +0200

    P2_1Item

---
+++ P2_2ItemValues/Item.cs
@@ -0,0 +1,24 @@
+﻿using P2_2ItemValues;
+
+Item[] items = new Item[3];
+
+// Create and assign values to each Item instance
+items[0] = new Item { goldValue = 100 };
+items[1] = new Item { goldValue = 200 };
+items[2] = new Item { goldValue = 300 };
+// Print the goldValue of each Item
+for (int i = 0; i < items.Length; i++)
+{
+    Console.WriteLine($"Item {i + 1}: Gold Value = {items[i].goldValue}");
+}
+
+namespace P2_2ItemValues
+{
+    public class Item
+    {
+        public int goldValue;
+
+    }
+
+// Declare an array of Item instances
+}
