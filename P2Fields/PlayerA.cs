0086bd26ed4293989723007ac031cabfbd659c25
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-10-10 13:18:58 +0200

    P2FieldsComplete

---
+++ P2Fields/PlayerA.cs
@@ -0,0 +1,35 @@
+﻿namespace P2Fields;
+
+public class PlayerA
+{
+    public string name ="Marc";
+    public string email = "marc@zaku.de";
+}
+public class Resource
+{
+    public int gold= 200;
+    public int stone= 110;
+    public int wood= 53;
+}
+
+public class Level
+{
+    public int level=12;
+}
+
+public class Experience
+{
+    public int experience = 123;
+}
+public class PvPMode
+{
+    public bool disabled = true;
+    public bool enablded = true;
+}
+public class VIPSubscription
+{
+    public bool active = true;
+    public bool inactive = true;
+}
+
+
--- P2Fields/Program.cs
+++ P2Fields/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using P2Fields;
+
+PlayerA playerInfo = new PlayerA();
+Console.WriteLine("Player A");
+Console.WriteLine($"Name: {playerInfo.name}");
+Console.WriteLine($"Email: {playerInfo.email}");
+
+Resource inventory =new Resource();
+Console.WriteLine("Resources");
+Console.WriteLine($"Gold:[{inventory.gold}] Stone:[{inventory.stone}] Wood:[{inventory.wood}]");
+
+Level playerLevel = new Level();
+Console.WriteLine($"Level: {playerLevel.level}");
+
+Experience exp= new Experience();
+Console.WriteLine($"Total EXP: {exp.experience}");
+
+PvPMode pvpMode = new PvPMode();
+Console.WriteLine("Do you want to turn on PvP mode? (y/n");
+string pvpInput =Console.ReadLine();
+if (pvpInput == "y")
+    Console.WriteLine($"PvP Mode [Enabled] {pvpMode.enablded}");
+else if (pvpInput =="n")
+    Console.WriteLine($"PvP Mode [Disabled]{pvpMode.disabled}");
+
+VIPSubscription status = new VIPSubscription();
+Console.WriteLine("Do you want to renew VIP Subscription?");
+string vipSubscription = Console.ReadLine();
+
+if (vipSubscription == "y")
+    Console.WriteLine($"PvP Mode [Active]:{status.active}");
+else if (vipSubscription =="n")
+    Console.WriteLine($"PvP Mode [Disabled]:{status.inactive}");
+
+
+
