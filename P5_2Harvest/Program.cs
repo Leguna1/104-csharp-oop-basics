024bb0a61ff26721b966ca2fbffb4de1646c2647
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-10-12 11:47:59 +0200

    P5_2HarvestDone

---
+++ P5_2Harvest/Program.cs
@@ -0,0 +1,9 @@
+﻿using P5_2Harvest;
+
+Plant plant =new Plant(); //Instrance
+Grain grain =new Grain(); //Instance
+Wheat wheat =new Wheat(); //Instance
+
+plant.CanGrow(); //Invoke method
+grain.CanBeHarvested(); //Invoke method
+wheat.CanBeProcessedToBread(); //Invoke method
