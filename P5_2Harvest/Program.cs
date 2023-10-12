354b5306189f34241a536b7cf1dacba8ec9a5d7a
AbdulakhKurbanov <lankurbanov@gmail.com>
2023-10-12 11:54:52 +0200

    AddedAllPossibleMethods

--- P5_2Harvest/Program.cs
+++ P5_2Harvest/Program.cs
@@ -4,6 +4,14 @@ Plant plant =new Plant(); //Instrance
 Grain grain =new Grain(); //Instance
 Wheat wheat =new Wheat(); //Instance
 
+Console.WriteLine("Invoking Plant class method..");
 plant.CanGrow(); //Invoke method
-grain.CanBeHarvested(); //Invoke method
-wheat.CanBeProcessedToBread(); //Invoke method
+
+Console.WriteLine("Invoking Grain class methods..");
+grain.CanGrow();//Invoke method
+grain.CanBeHarvested(); 
+
+Console.WriteLine("Invoking Wheat class methods..");
+wheat.CanBeHarvested(); //Invoke method
+wheat.CanBeProcessedToBread(); 
+
